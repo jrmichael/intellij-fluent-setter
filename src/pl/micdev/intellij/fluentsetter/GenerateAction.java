b656c61041247832be3a06834a4255d30c489f4c
Michal Bareja <michael.bareja@gmail.com>
2013-07-21 14:48:36 +0200

    first version implemented

--- src/pl/micdev/intellij/fluentsetter/GenerateAction.java
+++ src/pl/micdev/intellij/fluentsetter/GenerateAction.java
@@ -1,10 +1,73 @@
 package pl.micdev.intellij.fluentsetter;
 
+import java.util.List;
+
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.util.PsiTreeUtil;
 
 public class GenerateAction extends AnAction {
+
+    @Override
     public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
+        PsiClass psiClass = getPsiClass(e);
+        SelectFieldsDialog dialog = new SelectFieldsDialog(psiClass);
+        dialog.show();
+        if (dialog.isOK()) {
+            generateSetters(psiClass, dialog.getSelectedFields());
+        }
     }
+
+    private void generateSetters(final PsiClass psiClass, final List<PsiField> fields) {
+        new WriteCommandAction.Simple(psiClass.getProject(), psiClass.getContainingFile()) {
+            @Override
+            protected void run() throws Throwable {
+                for (PsiField field : fields) {
+                    PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(getProject());
+                    PsiMethod setter = elementFactory.createMethodFromText(generateSetterText(field, psiClass), psiClass);
+                    psiClass.add(setter);
+                }
+            }
+        }.execute();
+    }
+
+    private String generateSetterText(PsiField field, PsiClass psiClass) {
+        StringBuilder setterBuilder = new StringBuilder();
+        setterBuilder.append("public ").append(psiClass.getName()).append(" ").append(field.getName()).append('(').append(field
+                .getType().getPresentableText()).append(' ').append(field.getName()).append(") {\n");
+        setterBuilder.append("this.").append(field.getName()).append(" = ").append(field.getName()).append(";\n");
+        setterBuilder.append("return this;\n");
+        setterBuilder.append("}\n");
+        return setterBuilder.toString();
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        PsiClass psiClass = getPsiClass(e);
+        e.getPresentation().setEnabled(psiClass != null);
+    }
+
+    private PsiClass getPsiClass(AnActionEvent e) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        if (editor == null && psiFile == null) {
+            return null;
+        }
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement psiElement = psiFile.findElementAt(offset);
+        PsiClass psiClass = PsiTreeUtil.getParentOfType(psiElement, PsiClass.class);
+        return psiClass;
+    }
+
 }
---
+++ src/pl/micdev/intellij/fluentsetter/SelectFieldsDialog.java
@@ -0,0 +1,70 @@
+package pl.micdev.intellij.fluentsetter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.*;
+
+import org.jetbrains.annotations.Nullable;
+
+import com.intellij.ide.util.DefaultPsiElementCellRenderer;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.LabeledComponent;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiMethod;
+import com.intellij.ui.CollectionListModel;
+import com.intellij.ui.ToolbarDecorator;
+
+public class SelectFieldsDialog extends DialogWrapper {
+
+    private CollectionListModel<PsiField> fields;
+
+    private LabeledComponent<JPanel> component;
+
+    public SelectFieldsDialog(PsiClass psiClass) {
+        super(psiClass.getProject());
+        setTitle("Select fields");
+
+        prepareComponent(psiClass);
+
+        init();
+    }
+
+    private void prepareComponent(PsiClass psiClass) {
+        fields = new CollectionListModel<PsiField>(findFieldsWithoutFluentSetters(psiClass));
+
+        JList fieldList = new JList(fields);
+        fieldList.setCellRenderer(new DefaultPsiElementCellRenderer());
+        ToolbarDecorator decorator = ToolbarDecorator.createDecorator(fieldList);
+        decorator.disableAddAction();
+        JPanel panel = decorator.createPanel();
+        component = LabeledComponent.create(panel, "fields to generate setters for:");
+    }
+
+    private List<PsiField> findFieldsWithoutFluentSetters(PsiClass psiClass) {
+        Set<String> methods = new HashSet<String>();
+        for (PsiMethod psiMethod : psiClass.getMethods()) {
+            methods.add(psiMethod.getName());
+        }
+        List<PsiField> fieldsWithoutSetters = new ArrayList<PsiField>();
+        for (PsiField psiField : psiClass.getFields()) {
+            if(!methods.contains(psiField.getName()))
+                fieldsWithoutSetters.add(psiField);
+        }
+
+        return fieldsWithoutSetters;
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        return component;
+    }
+
+    public List<PsiField> getSelectedFields() {
+        return fields.getItems();
+    }
+}
