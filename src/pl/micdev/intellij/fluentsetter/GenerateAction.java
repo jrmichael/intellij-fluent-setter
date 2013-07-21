a93c86f390f3dc6d1e197a56c85ca383cf4f006e
Michal Bareja <michael.bareja@gmail.com>
2013-07-21 12:52:21 +0200

    Empty action added

---
+++ src/pl/micdev/intellij/fluentsetter/GenerateAction.java
@@ -0,0 +1,10 @@
+package pl.micdev.intellij.fluentsetter;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class GenerateAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
